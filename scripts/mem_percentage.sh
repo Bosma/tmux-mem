05e807ac44f5c23cfb6a30c4208940dabb397633
bosma <bosma@users.noreply.github.com>
2016-12-06 12:07:12 -0800

    fixed %

--- scripts/mem_percentage.sh
+++ scripts/mem_percentage.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_mem_percentage() {
-  free | grep Mem | awk '{printf(" %.2f%", $3/$2 * 100.0)}'
+  free | grep Mem | awk '{printf(" %.2f%%", $3/$2 * 100.0)}'
 }
 
 main() {
